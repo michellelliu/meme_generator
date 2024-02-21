ca094590d678527e8d0da3fb20b67673f8b5f95d
michellelliu <137734177+michellelliu@users.noreply.github.com>
2024-02-22 12:12:38 +1300

    allow users to customize text (font/colour/size)
    added combo and slider widgets to allow users to customize the text font, colour and size in the app

--- meme_generator.py
+++ meme_generator.py
@@ -1,6 +1,6 @@
 8#-------------IMPORTS------------------
 
-from guizero import App, TextBox, Drawing
+from guizero import App, TextBox, Drawing, Combo, Slider
 
 #-------------FUNCTIONS------------------
 def draw_meme():
@@ -8,20 +8,34 @@ def draw_meme():
     meme.image(50, 0, "whatt.gif")
     
     meme.text(90, 20, top_text.value,
-              color="white",
-              size="30",
-              font = "impact")
+              color=color.value,
+              size= size.value,
+              font = font.value)
     
     meme.text(90, 320, bottom_text.value,
-              color="white",
-              size="30",
-              font = "impact")
+              color=color.value,
+              size= size.value,
+              font = font.value)
 
 #-------------APP------------------
 app = App("meme")
 
+app.bg = "white"
+
 top_text = TextBox(app, "top text", command=draw_meme)
 bottom_text = TextBox(app, "bottom text", command=draw_meme)
+
+color = Combo(app,
+              options = ["black", "white", "red", "green", "blue", "orange"],
+              command = draw_meme,
+              selected = "white")
+
+font = Combo(app,
+             options = ["times new roman", "verdana", "courier", "impact"],
+             command = draw_meme,
+             selected = "impact")
+
+size = Slider(app, start=20, end=40, command=draw_meme)
 
 meme = Drawing(app, width="fill", height="fill")
 
