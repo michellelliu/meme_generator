b76b8e199a739f9c4820be0375f656425920a52e
michellelliu <137734177+michellelliu@users.noreply.github.com>
2024-02-22 11:47:21 +1300

    image and font changed
    -changed position of meme image to be centered
    -changed font, font size and font colour for better readability and contrast

--- meme_generator.py
+++ meme_generator.py
@@ -1,19 +1,27 @@
-#-------------IMPORTS------------------
+8#-------------IMPORTS------------------
 
 from guizero import App, TextBox, Drawing
 
 #-------------FUNCTIONS------------------
 def draw_meme():
     meme.clear()
-    meme.image(0, 0, "jasonm.gif")
-    meme.text(20, 20, top_text.value)
-    meme.text(20, 320, bottom_text.value)
+    meme.image(50, 0, "whatt.gif")
+    
+    meme.text(90, 20, top_text.value,
+              color="white",
+              size="30",
+              font = "impact")
+    
+    meme.text(90, 320, bottom_text.value,
+              color="white",
+              size="30",
+              font = "impact")
 
 #-------------APP------------------
 app = App("meme")
 
-top_text = TextBox(app, "top text")
-bottom_text = TextBox(app, "bottom text")
+top_text = TextBox(app, "top text", command=draw_meme)
+bottom_text = TextBox(app, "bottom text", command=draw_meme)
 
 meme = Drawing(app, width="fill", height="fill")
 
